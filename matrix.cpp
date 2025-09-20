68bdc65e8b31ed859101c89e5b3d3ce9117c1ead
ujjawal06-star <ujjwal9197us@gmail.com>
2025-09-20 22:45:02 +0530

    matrix in vector in cpp

---
+++ matrix.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<vector<int>> mat = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    cout << "2D Vector:" << endl;
+    for (int i = 0; i < mat.size(); i++) {
+        for (int j = 0; j < mat[i].size(); j++) {
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
